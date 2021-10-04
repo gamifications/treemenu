5c85ee0dc7fb04a9fa89389774ddde12d8df5382
suhailvs <suhailvs@gmail.com>
2021-10-04 23:14:26 +0530

    added saving functionality

--- guide/views.py
+++ guide/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from guide.models import Category
@@ -7,6 +6,15 @@ from guide.models import Category
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        messages.warning(request, 'Saved Success fulley')
-
+        if request.POST.get('parent_id') and request.POST.get('category_name'):
+            parent = request.POST['parent_id']
+            cat = request.POST['category_name']
+            if parent=='0':
+                Category.add_root(name=cat)
+            else:
+                Category.objects.get(pk=parent).add_child(name=cat)
+            messages.success(request, f'Success: Category {cat} created successfully.')
+        else:
+            messages.error(request, 'Error: Both Parent and Category Name is required.')
+        return redirect('home')
     return render(request,'home.html',{'menus':Category.dump_bulk(),'alist': Category.get_annotated_list()})
