6e4087c23e3e7eb2d0d15ff4c2d2cc7d4909c57a
suhailvs <suhailvs@gmail.com>
2021-10-04 22:52:09 +0530

    added database

--- guide/views.py
+++ guide/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib import messages
+
+from guide.models import Category
+
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        messages.warning(request, 'Saved Success fulley')
 
-    return render(request,'home.html')
+    return render(request,'home.html',{'menus':Category.dump_bulk(),'alist': Category.get_annotated_list()})
